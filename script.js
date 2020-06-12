eba2cffb835ef3960db2308ffd2585d956f47c80
Maciej Sznurawa <msznurawa@gmail.com>
2020-06-12 20:36:23 +0200

    Add transitions to hamburger and mobile menu

--- script.js
+++ script.js
@@ -11,12 +11,17 @@ const hamburgerButton = document.querySelector('.hamburger-menu');
 const mobileMenu = document.querySelector('.menu');
 
 const showMobileMenu = () => {
-  mobileMenu.classList.toggle('hidden');
+  mobileMenu.classList.toggle('visible');
+};
+
+const hamburgerActive = () => {
+  hamburgerButton.classList.toggle('hamburger-active');
 };
 
 
 hamburgerButton.addEventListener('click', (e) => {
   showMobileMenu();
+  hamburgerActive();
 });
 
 const mobileMenuItems = document.querySelectorAll('.menu__list-item.to-section a');
